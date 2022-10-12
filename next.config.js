3337395ac9169fbe7b95e5abc4ec930fdc90a5cb
kidow <wcgo2ling@gmail.com>
2022-10-12 11:43:12 +0900

    파일 정리

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  reactStrictMode: false,
+  swcMinify: true
 }
 
 module.exports = nextConfig
