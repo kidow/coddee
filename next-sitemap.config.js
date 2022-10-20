380895ee6cc2482fe1d06843c13777d01dae9d43
kidow <wcgo2ling@gmail.com>
2022-10-20 12:15:41 +0900

    sitemap 생성

---
+++ next-sitemap.config.js
@@ -0,0 +1,26 @@
+const { createClient } = require('@supabase/supabase-js')
+
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://coddee.dev',
+  generateRobotsTxt: true,
+  exclude: ['/404', '/500'],
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', disallow: ['/404', '/500'] }]
+  },
+  additionalPaths: async () => {
+    const { data, error } = await supabase.from('rooms').select('id')
+    if (error) return []
+    return data.map((item) => ({
+      loc: `/room/${item.id}`,
+      changefreq: 'daily',
+      priority: 0.7,
+      lastmod: new Date().toISOString()
+    }))
+  }
+}
