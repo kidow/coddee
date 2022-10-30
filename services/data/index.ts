eda645adefe131ced9d525631634e5b7e62ee5ae
kidow <wcgo2ling@gmail.com>
2022-10-30 19:45:04 +0900

    CHatMessage로 분리

--- services/data/index.ts
+++ services/data/index.ts
@@ -1,6 +1,6 @@
 export enum TOAST_MESSAGE {
   SESSION_EXPIRED = '세션이 만료되었습니다. 로그인을 다시 해주세요.',
-  API_ERROR = '에러가 발생했습니다. 문제가 지속된다면 문의바랍니다.',
+  API_ERROR = '에러가 발생했습니다. 문제가 지속된다면 문의부탁드립니다.',
   LOGIN_REQUIRED = '로그인이 필요합니다.'
 }
 
