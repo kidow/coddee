4d21941271639ffa0da601aeeb10b0e50cd66e76
kidow <wcgo2ling@gmail.com>
2022-11-05 17:01:31 +0900

    404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,20 @@
+import { SEO } from 'components'
 import type { NextPage } from 'next'
 
 interface State {}
 
 const NotFoundPage: NextPage = () => {
-  return <>NotFoundPage</>
+  return (
+    <>
+      <SEO title="Not Found" />
+      <div className="flex h-full items-center justify-center">
+        <div className="select-none text-center">
+          <div className="mt-1 mb-2 text-8xl text-neutral-700">404</div>
+          <div className="text-sm text-neutral-400">잘못 찾아오셨어요.</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default NotFoundPage
